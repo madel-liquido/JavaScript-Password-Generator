d1b9c527995cf1cedd5b35a1d2f55041ef05427f
Madel Liquido <madel.liquido@gmail.com>
2020-01-16 00:45:41 -0500

    Finalized password generator. Incorporated conditional to loop back to length question if the input is not a number that ranges from 8 to 128 characters.Also made remediations such that the new password replaces the previous password in the text area. Lastly, should the user enter a password length but cancel all the prompts, an alert pops up informing the user that a password could not be generated because no criteria were specified.

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var min = 8;
 var max = 128;
 
-var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-var upperChars = lowerChars.toUpperCase();
-var numberChars = "0123456789";
-var specialChars = "!#$%&()*+,-./:;<=>?@[^_{|}~";
-var guaranteedChars = [];
-var chars = [];
-var characters = "";
-var password = "";
-
-
-//need to include while loop for question
-//clear the box after each password
-
-
-
 function generatePassword() {
     var passwordLengthPrompt = prompt("Please specify password length (must be at least 8 characters and no more than 128 characters)");
     var passwordLength = parseInt(passwordLengthPrompt);
+
+    var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    var upperChars = lowerChars.toUpperCase();
+    var numberChars = "0123456789";
+    var specialChars = "!#$%&()*+,-./:;<=>?@[^_{|}~";
+
+    var guaranteedChars = [];
+    var possibleChars = "";
+    var password = "";
+
     document.getElementById("password").value = ""
     if (passwordLength >= min && passwordLength <= max && passwordLength !== NaN) {
         if (confirm("Does your password require lowercase characters?")) {
-            characters += lowerChars;
+            possibleChars += lowerChars;
             guaranteedChars.push(lowerChars[Math.floor(Math.random() * lowerChars.length)]);
         }
 
         if (confirm("Does your password require uppercase characters?")) {
-            characters += upperChars;
+            possibleChars += upperChars;
             guaranteedChars.push(upperChars[Math.floor(Math.random() * upperChars.length)]);
         }
 
         if (confirm("Does your password require a number?")) {
-            characters += numberChars;
+            possibleChars += numberChars;
             guaranteedChars.push(numberChars[Math.floor(Math.random() * numberChars.length)]);
         }
 
         if (confirm("Does your password require a special character?")) {
-            characters += specialChars;
+            possibleChars += specialChars;
             guaranteedChars.push(specialChars[Math.floor(Math.random() * specialChars.length)]);
         }
+        console.log(guaranteedChars);
 
     } else {
         alert("Please enter a valid password length.");
+        var max_count = 5;
+        if (--max_count > 0) {
+            generatePassword()
+        }
     }
 
-    charArray = characters.split("");
-    console.log(charArray);
-
-
+    var charArray = possibleChars.split("");
     for (var i = 0; i < passwordLength; i++) {
         var elementOne = charArray[Math.floor(Math.random() * charArray.length)];
         password += elementOne;
@@ -61,26 +57,24 @@ function generatePassword() {
         passwordArray[i] = guaranteedChars[i]
     }
 
-    var finalPassword = passwordArray.join('');
+    if (guaranteedChars.length !== 0) {
+        var finalPassword = passwordArray.join('');
+    } else {
+        alert("Could not generate password because no criteria were specified.");
+        var finalPassword = document.getElementById("password").defaultValue;
+    }
     return finalPassword;
 }
 
-//concatenate possible characters with guaranteed
-
-
-// Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//Add password to textbox/display area
 document.getElementById("password").value = generatePassword();
 
 
-//create separate arrays with guaranteed characters based on prompt
+
+
